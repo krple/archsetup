61dd17ebf1f99849c464cd48d3fb94459ddc7bd5
kurple <84683430+krple@users.noreply.github.com>
2025-03-25 15:14:57 -0500

    Update removeuneeded.sh

--- removeuneeded.sh
+++ removeuneeded.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Update package database
+sudo pacman -Sy
+
+# Remove vim, vim-runtime, and xterm
+sudo pacman -Rns vim xterm
+
+# Optionally, clean up unused packages
+sudo pacman -Rns $(pacman -Qdtq)
+
+echo "vim and xterm have been removed."
